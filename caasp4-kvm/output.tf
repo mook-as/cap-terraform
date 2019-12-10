f1613528fda0fca09fb4b79c01892ab2da1a932f
Sebastian Vollath <svollath@suse.com>
2019-12-10 15:33:21 +0100

    Fix output to depend on lb-reboot and add lb-name

--- caasp4-kvm/output.tf
+++ caasp4-kvm/output.tf
@@ -1,20 +1,23 @@
 data "external" "external_interface" {
-  program = ["/bin/bash", "scripts/get_external_ip.sh"]
-  query = {
-    id = libvirt_domain.lb.name
+  depends_on = [null_resource.lb_reboot]
+  program = ["/bin/bash", "scripts/get_external_ip.sh", libvirt_domain.lb.name]
   }
-}
 
 output "cluster_stack_name" {
   value = var.stack_name
 }
 
-output "ip_load_balancer-internal" {
-  value = libvirt_domain.lb.network_interface[0].addresses[0]
+output "ip_load_balancer-fqdn" {
+  value = format("%s-lb.cap.suse.de", var.stack_name)
 }
 
 output "ip_load_balancer-external" {
-  value = "${element(split(",", data.external.external_interface.result["address"]), 0)}" 
+  value = "${element(split(",", data.external.external_interface.result["address"]), 0)}"
+  depends_on = [null_resource.lb_reboot]
+}
+
+output "ip_load_balancer-internal" {
+  value = libvirt_domain.lb.network_interface[0].addresses[0]
 }
 
 output "ip_masters" {
@@ -23,5 +26,3 @@ output "ip_masters" {
 
 output "ip_workers" {
   value = [libvirt_domain.worker.*.network_interface.0.addresses.0]
-}
-
